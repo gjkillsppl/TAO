44eb614b24dab4022314fdec19dac98cd9e397c4
gjkillsppl <amin@belerofon.com.mx>
2019-04-17 00:35:42 -0500

    first commit

---
+++ menu.php
@@ -0,0 +1,97 @@
+<!-- Mobile Only Navigation
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <nav class="mobile-nav ">
+              <ul class="slimmenu font1">
+                <li>
+                    <a class="sub-collapser" href="">Portafolio</a>
+                    <ul>
+                      <li><a href="index.html">portfolio</a></li>
+                      <li><a href="index02.html">slider</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="sub-collapser" href="">portfolio</a>
+                    <ul>
+                        <li><a href="works.html">2 col</a></li>
+                        <li><a href="works02.html">3 col</a></li>
+                        <li><a href="works03.html">4 col</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="sub-collapser" href="">pages</a>
+                    <ul>
+                      <li><a href="about01.html">studio</a></li>
+                      <li><a href="about02.html">personal</a></li>
+                      <li><a href="elements.html">elements</a></li>
+                      <li><a href="news.html">news list</a></li>
+                      <li><a href="post.html">news post</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.html">Contacto</a></li>
+              </ul>
+  </nav>
+  <!-- mobile only navigation : ends -->
+
+
+
+  <!-- Header
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+  <section class="logo-header">
+
+        <div class="logo-block">
+          <a href="index.php"><img alt="" title="" class="img-responsive" src="images/logo.png"></a>
+        </div>
+
+
+        <div class="social-block">
+          <ul>
+            <li><a href="#"><span class="ion-social-twitter black"></span></a></li>
+            <li><a href="#"><span class="ion-social-facebook black"></span></a></li></li>
+          </ul>
+        </div>
+
+  </section>
+
+  <header class="masthead ">
+    <div class="container">
+      <div class="row">
+        <!-- <article class="col-md-3">
+          <a href="index.html"><img alt="" title="" class="main-logo " src="images/logo.png"></a>
+        </article> -->
+        <article class="col-md-6 text-left no-pad">
+          <nav class="mastnav ">
+
+                <ul class="main-menu text-left clearfix font2" ">
+                  <li>
+                      <a class="main-link font1 hvr-sweep-to-right activelink" href="#">Portafolio</a>
+                  </li>
+                  <li>
+                      <a class="main-link font1 hvr-sweep-to-right" href="about01.html">Taller</a>
+                  </li>
+                  <li><a class="main-link font1 hvr-sweep-to-right" href="contact.html">Contacto</a></li>
+                </ul>
+          </nav>
+
+        </article>
+        <article class="col-md-6 no-pad">
+            <!--works filter panel :starts -->
+              <!-- <div class="works-filter-wrap">
+                  <ul class="works-filter  text-right clearfix font2">
+                      <li><a id="all" href="#" data-filter="*" class="active"><span>All</span></a></li><li><a href="#" data-filter=".branding"><span>Branding</span></a></li>
+                      <li><a href="#" data-filter=".graphics"><span>Graphics</span></a></li>
+                      <li><a href="#" data-filter=".logos"><span>Logos</span></a></li>
+                      <li><a href="#" data-filter=".ui"><span>UI</span></a></li>
+                      <li><a href="#" data-filter=".web"><span>Web</span></a></li>
+                  </ul>
+            </div> -->
+          <!-- works filter panel :ends -->
+
+        </article>
+      </div>
+    </div>
+  </header>
+  <!-- end : masthead -->
+
+
+  <!-- MASTER CONTENT -->
